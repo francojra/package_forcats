c51e9b0a139251750dd8ca711baeced3e8dca9cf
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-03-06 23:56:49 -0300

    Add files via upload

--- package_forcats.R
+++ package_forcats.R
@@ -18,3 +18,18 @@
 library(forcats)
 library(ggplot2)
 library(dplyr)
+
+# O que são fatores ------------------------------------------------------------------------------------------------------------------------
+
+## fatores são uma classe de objetos no R criada para representar as variáveis categóricas numericamente.
+## Eles são necessários pois muitas vezes precisamos representar variáveis categóricas como números.
+## Quando estamos fazendo um gráfico, por exemplo, só podemos mapear variáveis numéricas em seus eixos,
+## pois o plano cartesiano é formado por duas retas de números reais.
+## O que fazemos então quando plotamos uma variável categórica? Nós a transformamos em fatores.
+
+## Mas como a manipulação de fatores é diferente da manipulação de números e strings (graças aos famosos levels),
+## tarefas que parecem simples, como ordenar as barras de um gráfico de barras, acabam se tornando grandes 
+## desafios quando não sabemos lidar com essa classe de valores.
+
+## Nos exemplos a seguir, vamos utilizar a base starwars (do pacote {dplyr}) para aprendermos a fazer as 
+## principais operações com fatores utilizando o pacote {forcats}.
